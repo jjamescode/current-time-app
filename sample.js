2db5ddb8c1f4170924c08f72e3969ecb3d334989
jjamescode <jalandajames@yahoo.com>
2022-03-29 23:49:22 -0500

    Initial Commit

---
+++ sample.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+      function getDate(){
+        var cDate=new Date();
+        document.getElementById("Date").innerHTML = cDate.toString();
+      }
+    </script>
